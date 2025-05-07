ce67e9a2838f289e7c43f04c464d158e8b588590
“xilin777” <2356310245@qq.com>
2025-05-08 01:50:07 +0800

    Define the function for sending requests

--- client.py
+++ client.py
@@ -1,2 +1,9 @@
 import socket
 import sys
+
+def send_request(sock, request):
+    size = len(request) + 3  
+    formatted_request = f"{size:03d}{request}"  
+    sock.send(formatted_request.encode())  
+    response = sock.recv(1024).decode()  
+    print(f"Request: {request}, Response: {response}")  
