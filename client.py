79c275d1a0bdcf074b9a8ecd31cad73491ff2f4f
“xilin777” <2356310245@qq.com>
2025-05-08 01:54:34 +0800

    Main program entry

--- client.py
+++ client.py
@@ -6,4 +6,9 @@ def send_request(sock, request):
     formatted_request = f"{size:03d}{request}"  
     sock.send(formatted_request.encode())  
     response = sock.recv(1024).decode()  
-    print(f"Request: {request}, Response: {response}")  
+    print(f"Request: {request}, Response: {response}") 
+    
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: python client.py <host> <port> <request_file>")
+        sys.exit(1) 
