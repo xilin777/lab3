a85af057aecfac40e7d42a3530889c700a0cb4a1
“xilin777” <2356310245@qq.com>
2025-05-07 22:30:28 +0800

    commit

--- server.py
+++ server.py
@@ -5,3 +5,19 @@ import time
 tuplespace = {} 
 operationcount = {'total': 0, 'read': 0, 'get': 0, 'put': 0, 'error': 0} 
 clientcount = 0 
+
+def handleclient(clientsocket):
+    global operationcount
+    global clientcount
+    clientcount += 1 
+     
+    try:
+        while True:
+            data = clientsocket.recv(1024).decode()  
+            if not data: 
+                break
+            operationcount['total'] += 1 
+            size = int(data[:3])  
+            command = data[3] 
+            
+            
