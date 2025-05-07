61689acfa560b1e58b6a9557d7c89e81b7ee8ebe
“xilin777” <2356310245@qq.com>
2025-05-07 22:46:56 +0800

    commit

--- server.py
+++ server.py
@@ -19,4 +19,24 @@ def handleclient(clientsocket):
             size = int(data[:3])  
             command = data[3] 
             
+            if command == 'R': 
+                operationcount['read'] += 1 
+                key = data[5:]  
+                if key in tuplespace:  
+                    value = tuplespace[key]  
+                    response = f"{len(f'OK ({key}, {value}) read'):03d} OK ({key}, {value}) read"
+                else:  
+                    operationcount['error'] += 1  
+                    response = f"{len('ERR no such key'):03d} ERR no such key" 
+                    
+            elif command == 'G': 
+                operationcount['get'] += 1 
+                key = data[5:]  
+                if key in tuplespace:  
+                    value = tuplespace.pop(key)  
+                    response = f"{len(f'OK ({key}, {value}) get'):03d} OK ({key}, {value}) get"
+                else:  
+                    operationcount['error'] += 1 
+                    response = f"{len('ERR no such key'):03d} ERR no such key"           
+            
             
